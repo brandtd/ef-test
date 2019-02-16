b2ce20bc9e05712baa284ed9f1cb2824bf94370a
brandtd <brandt.dtb+git@gmail.com>
2019-02-16 18:46:38 -0500

    Initial commit

---
+++ Test1/MyDatabase.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Test1
+{
+    public class Lesson
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public RollList Roll { get; set; }
+    }
+
+    /// <summary>Join table to allow many-to-many between Lesson and Student</summary>
+    public class LessonPlan
+    {
+        public int Id { get; set; }
+        public ICollection<Lesson> Lessons { get; set; } = new List<Lesson>();
+        public Student Student { get; set; }
+        public int StudentId { get; set; } // Necessary to identify LessonPlan as child of Student?
+    }
+
+    public class MyContext : DbContext
+    {
+        public DbSet<LessonPlan> LessonPlans { get; set; }
+        public DbSet<Lesson> Lessons { get; set; }
+        public DbSet<RollList> RollLists { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseInMemoryDatabase("inmemory");
+        }
+    }
+
+    /// <summary>Join table to allow many-to-many between Lesson and Student</summary>
+    public class RollList
+    {
+        public int Id { get; set; }
+        public Lesson Lesson { get; set; }
+        public int LessonId { get; set; } // Necessary to identify RollList as child of Lesson?
+
+        public ICollection<Student> Students { get; set; } = new List<Student>();
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public LessonPlan LessonPlan { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Test1/Program.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace Test1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var db = new MyContext())
+            {
+                Lesson science = new Lesson { Name = "Science" };
+                Lesson math = new Lesson { Name = "Math" };
+                Lesson boring = new Lesson { Name = "Other Boring Stuff" };
+
+                Student jamie = new Student { Name = "Jamie" };
+                Student chris = new Student { Name = "Chris" };
+                Student danny = new Student { Name = "Danny" };
+
+                science.Roll = new RollList { Lesson = science };
+                math.Roll = new RollList { Lesson = math };
+                boring.Roll = new RollList { Lesson = boring };
+
+                jamie.LessonPlan = new LessonPlan { Student = jamie };
+                chris.LessonPlan = new LessonPlan { Student = chris };
+                danny.LessonPlan = new LessonPlan { Student = danny };
+
+                jamie.LessonPlan.Lessons.Add(science);
+                jamie.LessonPlan.Lessons.Add(math);
+                science.Roll.Students.Add(jamie);
+                math.Roll.Students.Add(jamie);
+
+                chris.LessonPlan.Lessons.Add(math);
+                chris.LessonPlan.Lessons.Add(boring);
+                math.Roll.Students.Add(chris);
+                boring.Roll.Students.Add(chris);
+
+                danny.LessonPlan.Lessons.Add(boring);
+                danny.LessonPlan.Lessons.Add(science);
+                boring.Roll.Students.Add(danny);
+                science.Roll.Students.Add(danny);
+
+                db.Lessons.Add(science);
+                db.Lessons.Add(math);
+                db.Lessons.Add(boring);
+                db.SaveChanges();
+            }
+
+            DoAThing t1 = new DoAThing(new MyContext());
+            DoAThing t2 = new DoAThing(new MyContext());
+            Console.WriteLine("Before any shenanigans");
+            t1.PrintIt();
+            t2.PrintIt();
+
+            Console.WriteLine();
+            Console.WriteLine("Changing name under one context");
+            t1.SetIt("new name");
+
+            Console.WriteLine();
+            Console.WriteLine("After shenanigans");
+            t1.PrintIt();
+            t2.PrintIt();
+
+            Console.ReadLine();
+        }
+
+        public static void PrintDatabase()
+        {
+            using (var db = new MyContext())
+            {
+                Console.WriteLine("Lessons:");
+                foreach (var lesson in db.Lessons)
+                {
+                    Console.WriteLine($"Lesson: id: {lesson.Id}, name: {lesson.Name}");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Students:");
+                foreach (var student in db.Students)
+                {
+                    Console.WriteLine($"Student: id: {student.Id}, name: {student.Name}");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Lesson Plans:");
+                foreach (var plan in db.LessonPlans)
+                {
+                    Console.WriteLine($"Lesson Plan: id: {plan.Id}");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Roll Lists:");
+                foreach (var roll in db.RollLists)
+                {
+                    Console.WriteLine($"Roll List: id: {roll.Id}");
+                }
+            }
+        }
+
+        public class DoAThing
+        {
+            public DoAThing(MyContext db)
+            {
+                _db = db;
+            }
+
+            public void PrintIt()
+            {
+                Console.WriteLine($"Name under context {_db.GetHashCode()}: {_db.Students.Where(s => s.Id == 2).First().Name}");
+            }
+
+            public void SetIt(string name)
+            {
+                Console.WriteLine($"Changing name under context {_db.GetHashCode()}");
+                _db.Students.Where(s => s.Id == 2).First().Name = name;
+                _db.SaveChanges();
+            }
+
+            private MyContext _db;
+        }
+    }
+}
